9fb3ae12e374de8bf4b6ef53cd9642a563ae7284
reeyounghyun <naem77716@gmail.com>
2024-07-27 12:56:29 +0900

    03회차 업데이트

---
+++ 03회차/my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let hi = '지금은 수업중';
+
+  function func1(){
+    console.log('click!');
+  }
+
+  return (
+    <>
+      <p className='red' style={{backgroundColor: 'yellow', fontSize:"30px"}}>{hi}</p> 
+      <div onClick={func1}>click</div>
+    </>
+  );
+}
+
+export default App;
